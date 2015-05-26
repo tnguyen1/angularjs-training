87dfd50a579a61ca3c23f987f479c12afa6008f2
Truc Nguyen <truc.nguyen@bonitasoft.com>
2015-05-26 16:59:09 +0200

    initial commit of poney-racer

---
+++ poney-racer/src/app/components/navbar/navbar.controller.js
@@ -0,0 +1,6 @@
+'use strict';
+
+angular.module('poneyRacer')
+  .controller('NavbarCtrl', function ($scope) {
+    $scope.date = new Date();
+  });
---
+++ poney-racer/src/app/index.js
@@ -0,0 +1,14 @@
+'use strict';
+
+angular.module('poneyRacer', ['ngCookies', 'ngRoute', 'ui.bootstrap'])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  })
+;
