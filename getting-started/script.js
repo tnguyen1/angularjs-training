5122fe7c083f44ad184ba7d07159106c21dbbdcf
Truc Nguyen <truc.nguyen@bonitasoft.com>
2015-05-26 13:58:02 +0200

    separate script.js + nested controller exercice

---
+++ getting-started/script.js
@@ -0,0 +1,16 @@
+var app = angular.module('app', ['ui.bootstrap']);
+
+app.controller('FormController', function($scope) {
+  $scope.formctrl = {};
+  $scope.formctrl.name = 'Truc';
+  $scope.formctrl.age = '30';
+  $scope.submit = function() {
+    $scope.formctrl.message = "[parent] Congratulations " + $scope.formctrl.name + ", you are " + $scope.formctrl.age + "!";
+  };
+});
+
+app.controller('NestedController', function($scope) {
+  $scope.submit = function() {
+    $scope.formctrl.message = "[child] Congratulations " + $scope.formctrl.name + "!";
+  };
+});
