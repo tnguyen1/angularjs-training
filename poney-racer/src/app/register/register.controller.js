02fef4339d1f6af036ad0b99d96e9a4a644098ff
Truc Nguyen <truc.nguyen@bonitasoft.com>
2015-05-28 10:12:09 +0200

    add button to register test users + order user list by login

--- poney-racer/src/app/register/register.controller.js
+++ poney-racer/src/app/register/register.controller.js
@@ -18,5 +18,29 @@ angular.module('controllers')
           $scope.registerError = true;
           $scope.registerErrorMessage = data.message;
         });
-    }
+    };
+
+    var createUser = function(firstname, lastname, email, birthYear, login, password) {
+      return {
+        'firstName': firstname,
+        'lastName': lastname,
+        'email': email,
+        'birthYear': birthYear,
+        'login': login,
+        'password': password
+      };
+    };
+
+    $scope.registerTestUsers = function() {
+      var testUsers = [];
+      testUsers.push(createUser('Roger', 'Federer', 'roger@itf.com', 1976, 'roger', 'a'));
+      testUsers.push(createUser('Raphael', 'Nadal', 'rafa@itf.com', 1986, 'rafa', 'a'));
+      testUsers.push(createUser('Jo Wilfried', 'Tsonga', 'jo@itf.com', 1985, 'jo', 'a'));
+      testUsers.push(createUser('Stanislas', 'Wawrinka', 'stan@itf.com', 1985, 'stan', 'a'));
+      testUsers.push(createUser('Novak', 'Djokovic', 'nole@itf.com', 1986, 'nole', 'a'));
+      for (var i = 0; i < testUsers.length; i++) {
+        $scope.register(testUsers[i]);
+      }
+    };
+
   });
